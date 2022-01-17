0d798cb1ccb8dc198c73c07d15643fd028b04b85
UlyssesPinheiro <ulyssespinheiro00@gmail.com>
2022-01-17 11:56:37 -0300

    removed fractional, reinstalled parcel

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,3 +1,4 @@
+import fracty from 'fracty';
 import View from './view.js';
 
 import icons from 'url:../../img/icons.svg';
@@ -124,7 +125,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      ing.quantity ? fracty(ing.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit ? ing.unit : ''}</span>
